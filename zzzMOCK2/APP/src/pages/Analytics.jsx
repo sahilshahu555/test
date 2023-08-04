9dd8c7daa93680a8435a5526c940fc48e78def16
Sahil Shahu <sahilshahu555@gmail.com>
2023-08-04 17:54:32 +0530

    edited

--- zzzMOCK2/APP/src/pages/Analytics.jsx
+++ zzzMOCK2/APP/src/pages/Analytics.jsx
@@ -1,35 +1,77 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
-import { Chart,ArcElement,Tooltip,Legend} from "chart.js"
-import {Pie}  from "react-chartjs-2";
-
-
-Chart.register(ArcElement,Tooltip,Legend);
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Pie } from 'react-chartjs-2';
+import {Chart as Chartjs} from "chart.js/auto"
+import { Box } from '@chakra-ui/react';
 
 const Analytics = () => {
-   const dataIncome={
-       labels:["one","two","three"],
-      datasets:[{data:[3,6,9], backgroundColor:["red","grey","skyblue"]}]
-   };
-   const dataExpense={
-    labels:["Four","five","six"],
-    datasets:[{data:[4,5,6], backgroundColor:["green","black","yellow"]}]
-   }
-   const options ={};
+  const [data, setData] = useState([]);
 
-   
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://abcd-zc44.onrender.com/Tracker');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+
+    function getRandomColors(length) {
+      const colors = [];
+      for (let i=0; i < length; i++) {
+        colors.push('#' + Math.floor(Math.random() *16777215).toString(16));
+      }
+      return colors;
+    }
+
+    const dataIc= data.filter((elem)=>{ return elem.type==="Income" })
+
+    const dataEx= data.filter((elem)=>{ return elem.type==="Expense" })
+
   return (
-    <Box style={{display:"flex",justifyContent:"space-evenly"}}>
-      <Box>
-          <h1 style={{fontSize:"28px" ,textAlign:"center",fontWeight:"bold"}}>Income</h1>
-          <Pie data={dataIncome} options={options}></Pie>
-      </Box>
-      <Box>
-        <h1 style={{fontSize:"28px" ,textAlign:"center",fontWeight:"bold"}}>Expenses</h1>
-          <Pie data={dataExpense} options={options}></Pie>
-      </Box>
-    </Box>
-  )
-}
+    <div style={{display:"flex",justifyContent:"space-evenly",alignItems:"center"}}>
+          <Box>
+              <h2>Income</h2>
+              {dataIc.length > 0 && (
+                <Pie 
+                  data={{
+                    labels: dataIc.map((item) => item.category),
+                    datasets: [{
+                        data: dataIc.map((item) => item.amount),
+                        backgroundColor: getRandomColors(dataIc.length)
+                              }],
+                  }}
+                />
+              )}
+          
+          </Box>
+          <Box>
+              <h2>Expense</h2>
+              {dataEx.length > 0 && (
+                <Pie 
+                  data={{
+                    labels: dataEx.map((item) => item.category),
+                    datasets: [{
+                        data: dataEx.map((item) => item.amount),
+                        backgroundColor: getRandomColors(dataEx.length)
+                              }],
+                  }}
+                />
+              )}
+          
+          </Box>
+    </div>
+  );
+};
+
+
+
+
 
 export default Analytics
+
+
