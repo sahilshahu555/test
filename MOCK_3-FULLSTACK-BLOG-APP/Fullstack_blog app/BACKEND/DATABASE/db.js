6722564fe7019f28063f935f03990b7819920a83
sahilshahu555 <sahilshahu555.com>
2023-08-17 05:07:03 -0700

    edited.

--- MOCK_3-FULLSTACK-BLOG-APP/Fullstack_blog app/BACKEND/DATABASE/db.js
+++ MOCK_3-FULLSTACK-BLOG-APP/Fullstack_blog app/BACKEND/DATABASE/db.js
@@ -1,5 +1,4 @@
 const mongoose=require("mongoose");
-
 const connection= mongoose.connect("mongodb+srv://sahilshahu555:harsh123123123@cluster0.ehyj2m8.mongodb.net/Mock3DB")
 
 module.exports={connection}
